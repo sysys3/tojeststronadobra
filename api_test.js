d6a083601e8adced11291d66b55bf6b9b41f5d17
sysys3 <114004936+sysys3@users.noreply.github.com>
2022-09-22 19:38:59 +0200

    Add files via upload

---
+++ api_test.js
@@ -0,0 +1,6 @@
+let response = fetch(api_url, [other params])
+
+async function funcName(url){
+    const response = await fetch(url);
+    var data = await response.json();
+    }
---
+++ skrypt.js
@@ -0,0 +1,26 @@
+// var f = output.txt
+
+//writeTextFile(f, "OK")
+//writeTextFile(f, "OK1")
+//writeTextFile(f, "ok3")
+
+//function writeTextFile(afilename, output)
+//{
+//    var txtFile = new File(afilename);
+//    txtFile.writeIn(output);
+//    txtFile.close();
+//}
+
+function buton() {
+    document.getElementById("przycisk").innerHTML = "To dobrze";
+}
+
+function buton1() {
+    document.getElementById("przycisk1").innerHTML = "To dobrze";
+}
+
+var delayInMilliseconds = 1000;
+setTimeout(function() {
+}, delayInMilliseconds);
+
+
